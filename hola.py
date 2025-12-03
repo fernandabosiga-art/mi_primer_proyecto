48e05834d070915aa729f3022fd8e4fab4e8bf45
Gina Bosiga <fernandabosiga@gmail.com>
2025-12-02 21:45:08 -0500

    Segunda versión: se muestra la longitud del nombre

---
+++ hola.py
@@ -0,0 +1,6 @@
+# hola.py
+# Autor: Gina Fernanda Bosiga
+
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}! Bienvenida a tu primer proyecto con Git y Python.")
+print(f"Tu nombre tiene {len(nombre)} letras.")
